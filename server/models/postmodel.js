dab87e2d61e6a9ce8dd33c58ff340ac8c3b0c1ca
leksyib <leksyib14@gmail.com>
2018-07-04 13:05:29 +0100

    setup models

--- server/models/postmodel.js
+++ server/models/postmodel.js
@@ -3,7 +3,7 @@ const schema = mongoose.Schema
 
 const post = new Schema({ 
   title: { type: String, required: true },
-  text: { type: string, required: true }
+  text: { type: String, required: true }
 },{
   timestamps: true
 })
